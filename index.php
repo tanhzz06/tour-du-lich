557b7829a9afe0e02564ba1258c2b5771f0d10af
DoXuanHieu <hieud8155@gmail.com>
2026-03-27 21:27:19 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
-session_start();
+<?php 
 
-require_once "./vendor/autoload.php";
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
 
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
+// Require toàn bộ file Controllers
+require_once './controllers/HomeController.php';
 
-require_once "./routes/web.php";
+// Require toàn bộ file Models
+require_once './models/Student.php';
+
+// Route
+$act = $_GET['act'] ?? '/';
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+
+match ($act) {
+    // Trang chủ
+    
+};
